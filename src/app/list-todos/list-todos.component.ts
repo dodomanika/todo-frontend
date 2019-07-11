31fa7f2585223fd7e8f1dd17aa47dbfb0a63a729
Dominika Piechota <piechota.domi@gmail.com>
2019-07-11 22:35:27 +0200

    MyTodos - connecting to todo api, showing the response from todo service on the page

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {TodoDataService} from '../service/data/todo-data.service';
 
-export class Todo{
+export class Todo {
   constructor(
     public id: number,
     public description: string,
     public done: boolean,
     public targetDate: Date
-  ){
+  ) {
 
   }
 }
@@ -18,24 +19,33 @@ export class Todo{
 })
 export class ListTodosComponent implements OnInit {
 
-  todos = [
+  todos: Todo[];
+  /*= [
     new Todo (1, 'Learn to dance', false, new Date()),
     new Todo (2, 'Become an expert at Angular', false, new Date()),
     new Todo (3, 'Visit India', false, new Date())
     // {id: 1, description: 'Learn to dance'},
     // {id: 2, description: 'Become an expert at Angular'},
     // {id: 3, description: 'Visit India'}
-  ]
+  ];*/
 
-  //an object
+  // an object
   // todo = {
   //   id: 1,
   //   description: 'Learn to dance'
   // }
 
-  constructor() { }
+  constructor(
+    private todoService: TodoDataService
+  ) { }
 
   ngOnInit() {
+    this.todoService.retrieveAllTodos('username').subscribe(
+      response =>{
+        console.log(response);
+        this.todos = response;
+      }
+    );
   }
 
 }
