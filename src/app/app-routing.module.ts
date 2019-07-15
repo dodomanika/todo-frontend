f6dd92465140ced0f91f94a160b47630f49ecc60
Dominika Piechota <piechota.domi@gmail.com>
2019-07-15 23:12:42 +0200

    MyTodos - todo component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { ErrorComponent } from './error/error.component';
-import { ListTodosComponent } from './list-todos/list-todos.component';
-import { LogoutComponent } from './logout/logout.component';
-import { RouteGuardService } from './service/route-guard.service';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {ErrorComponent} from './error/error.component';
+import {ListTodosComponent} from './list-todos/list-todos.component';
+import {LogoutComponent} from './logout/logout.component';
+import {RouteGuardService} from './service/route-guard.service';
+import {TodoComponent} from './todo/todo.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
@@ -13,6 +14,7 @@ const routes: Routes = [
   {path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService]},
   {path: 'todos', component: ListTodosComponent, canActivate: [RouteGuardService]},
   {path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService]},
+  {path: 'todos/{id}', component: TodoComponent, canActivate: [RouteGuardService]},
   {path: '**', component: ErrorComponent}
 ];
 
@@ -20,4 +22,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
