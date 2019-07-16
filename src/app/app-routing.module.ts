0a2f795c8b0296b97587229e4336029619ecb37b
Dominika Piechota <piechota.domi@gmail.com>
2019-07-16 15:22:38 +0200

    MyTodos - correcting the todo path in app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   {path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService]},
   {path: 'todos', component: ListTodosComponent, canActivate: [RouteGuardService]},
   {path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService]},
-  {path: 'todos/{id}', component: TodoComponent, canActivate: [RouteGuardService]},
+  {path: 'todos/:id', component: TodoComponent, canActivate: [RouteGuardService]},
   {path: '**', component: ErrorComponent}
 ];
 
